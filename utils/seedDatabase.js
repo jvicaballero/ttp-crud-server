41e783f925dbf4036d9239a5d1ea518cfd0b069e
Jasper Caballero <jvicaballero@gmail.com>
2020-06-13 01:39:04 -0400

    Added additional info in student model such as lastName, email with validation, gpa(range not sure)

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -17,8 +17,8 @@ const seedDatabase = async () => {
       address: "Manhattan",
       description: "A college in Manhattan",
     }),
-    Student.create({ firstName: "Daniel" }),
-    Student.create({ firstName: "Sally", campusId: 1 }),
+    Student.create({ firstName: "Daniel", lastName:'Smith',email:'danielSmith@gmail.com', gpa: '3.0',}),
+    Student.create({ firstName: "Sally", lastName:'Jensen',email:'sallyJensen@yahoo.com',gpa: '3.8', campusId: 1 }),
   ]);
 };
 
