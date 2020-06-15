2ec3430028f185287da4e999701dbc8a37a21569
Jasper Caballero <jvicaballero@gmail.com>
2020-06-15 02:11:06 -0400

    added put and delete routes for students

--- routes/students.js
+++ routes/students.js
@@ -59,4 +59,57 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-  module.exports = router;
+// Route to handle removing a student
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  // get an id for a student to delete
+  try {
+    // pass the id to the database to find student to be deleted
+    // database would either respond succcess or fail
+    const student = await Student.findByPk(id);
+    // invoke the .destroy() method on the returned student
+    await student.destroy();
+    // send a success message to the client
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Route to handle editing a student
+// /api/students/:id
+// /api/students/456 would modify a student with id 456
+router.put("/:id", async (req, res, next) => {
+  // get the id from request params
+  const { id } = req.params;
+  // get form data from the request body
+  const { firstName, lastName, email, gpa, imageUrl } = req.body;
+  const updatedObj = {
+    firstName,
+    lastName,
+    email,
+    gpa,
+    imageUrl,
+  };
+  try {
+    // if successfull:
+    // Find a Student with a matching id from the database
+    const student = await Student.findByPk(id);
+    // database would return a valid Student object or an error
+    console.log(updatedObj);
+    // modify the Student object with new form data
+    await student.set(updatedObj);
+    // save the new Student object to the data
+    // database would return a new Student object
+    const updatedStudent = await student.save();
+    console.log(updatedStudent);
+    // send the newStudent as a response from the API
+    res.status(201).send(updatedStudent);
+  } catch (err) {
+    // if error:
+    // handle the error
+    next(err);
+  }
+});
+
+module.exports = router;
